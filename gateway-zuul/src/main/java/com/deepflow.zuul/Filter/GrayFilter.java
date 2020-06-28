95bffcfb4aa60ee3409c9b76395b772ca039420b
xudejian <xdj@gato.com.cn>
2020-06-28 09:52:23 +0800

    zuul 实现灰度发布

---
+++ gateway-zuul/src/main/java/com/deepflow.zuul/Filter/GrayFilter.java
@@ -0,0 +1,49 @@
+package com.deepflow.zuul.Filter;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import io.jmnarloch.spring.cloud.ribbon.support.RibbonFilterContextHolder;
+
+
+@Configuration
+public class GrayFilter extends ZuulFilter {
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        // a filter has already forwarded
+        // a filter has already determined serviceId
+        return !ctx.containsKey(FORWARD_TO_KEY)
+                && !ctx.containsKey(SERVICE_ID_KEY);
+    }
+
+    @Override
+    public Object run()  {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        String mark = request.getHeader("gray_mark");
+        if(StringUtils.isNotEmpty(mark)&& "enable".equals(mark)){
+            RibbonFilterContextHolder.getCurrentContext().add("host-mark","gray-host");
+        }else{
+            RibbonFilterContextHolder.getCurrentContext().add("host-mark","running-host");
+        }
+        return null;
+    }
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER -1;
+    }
+}
--- gateway-zuul/src/main/java/com/deepflow.zuul/ZuulServerApplication.java
+++ gateway-zuul/src/main/java/com/deepflow.zuul/ZuulServerApplication.java
@@ -2,12 +2,11 @@ package com.deepflow.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableOAuth2Sso
+// @EnableOAuth2Sso
 public class ZuulServerApplication {
     public static void main(String[] args){
         SpringApplication.run(ZuulServerApplication.class,args);
